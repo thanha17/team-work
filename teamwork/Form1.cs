bb6960d1493d41bd8e2feeb002840ed8ad7bccdb
NPB <abcxyz@gmail.com>
2024-01-26 23:22:01 +0700

    BaoUpdate

--- teamwork/Form1.cs
+++ teamwork/Form1.cs
@@ -16,5 +16,11 @@ namespace teamwork
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //Bao
+            //ganhTeam
+        }
     }
 }
